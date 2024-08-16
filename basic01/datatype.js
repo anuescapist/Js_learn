76a19243290a88dc997609a181ec061f82f01f9c
anushka choudhary <93811079+anuescapist@users.noreply.github.com>
2024-08-16 23:58:20 +0000

    datatype explaination premitive

---
+++ basic01/datatype.js
@@ -0,0 +1,29 @@
+"use strict";// treat all js code as newwer version 
+// java script engine is inside broswer when write alert
+//when we are writing in browser we are actually using  B.E
+//most of the people bind js with broswer and show out put in console by using broswer engine
+
+
+//alert("hello"); this will give error because we are using node js not browser 
+
+//code need to readable thats most important thing
+
+//basicly there are only few data type 
+/*
+string _ normally whatever we write in (" ") treat like a string
+number _ its number 
+null_ means null is nothing
+undefined_ is something variable is there but not is still not defined
+symbol _ basically symbol is used for define the uniqueness
+null _ null will be always shown as a object when we try to typeoff
+*/
+
+let name = "anushka";
+let age = 18
+let marriage = false;
+
+let temp = null;
+
+console.table(typeof temp);// here you can see output is object
+console.log(typeof age);
+console.log(typeof marriage);
