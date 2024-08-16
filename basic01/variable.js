313f6cf6036fea66f156a97c346ac0022781098c
anushka choudhary <93811079+anuescapist@users.noreply.github.com>
2024-08-16 21:43:00 +0000

    variable testing

---
+++ basic01/variable.js
@@ -0,0 +1,14 @@
+const accountId = 123432;
+let accountEmail = "annuu088@gmail.com"
+var accountPassword = "34567"
+
+//const cant be changed
+//var could be changed
+//let is block scoped we will always use let not var for security purpose
+
+console.table([accountEmail,accountId,accountPassword]);
+
+//console table everything can be seen in table 
+  
+
+console.log(accountEmail,accountId,accountPassword)
